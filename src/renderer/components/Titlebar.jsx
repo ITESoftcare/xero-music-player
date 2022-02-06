69c9e9feb6314c2895cd618f6cbfcaca594e4123
Saurabh Tanwar <itesaurabh@gmail.com>
2022-02-06 22:39:33 +0530

    linux compat titlebar

--- src/renderer/components/Titlebar.jsx
+++ src/renderer/components/Titlebar.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ArrowBackOutlinedIcon from '@mui/icons-material/ArrowBackOutlined';
 import ArrowBackIosNewOutlinedIcon from '@mui/icons-material/ArrowBackIosNewOutlined';
-import { Box, Button } from '@mui/material';
+import { Box, Button, IconButton, Stack } from '@mui/material';
+import { sendMessageToNode } from '../../main/utils/renProcess';
 import { useNavigate } from 'react-router-dom';
 import { OS_WINDOWS, OS_MAC } from '../../config/constants';
 const os = require('os');
@@ -13,6 +14,12 @@ const Titlebar = () => {
         <>
             <Box className={currOs === OS_WINDOWS ? "title-bar title-bar_windows" : "title-bar title-bar_unix"}>
                 <div className="tb-controls">
+
+                        <div className="traffic-light">
+                            <div onClick={() => sendMessageToNode('closeWindow')} className="close-unix">close</div>
+                            <div onClick={() => sendMessageToNode('minimize')} className="mini-unix">minimise</div>
+                            <div onClick={() => sendMessageToNode('maximize')} className="res-unix">resize</div>
+                        </div>
                     <Box className={currOs === OS_MAC ? "mac_back" : "tb-cntrl_windows"}
                         sx={{ visibility: window.history.length === 1 ? 'hidden' : 'inherit' }}
                         onClick={() => navigate(-1)}
