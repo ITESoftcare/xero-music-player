691d1a01ba154d58493a8a4605ed74aa1f81fb21
Saurabh Tanwar <itesaurabh@gmail.com>
2022-02-06 22:48:06 +0530

    linux specific patch

--- src/renderer/components/Titlebar.jsx
+++ src/renderer/components/Titlebar.jsx
@@ -4,7 +4,7 @@ import ArrowBackIosNewOutlinedIcon from '@mui/icons-material/ArrowBackIosNewOutl
 import { Box, Button, IconButton, Stack } from '@mui/material';
 import { sendMessageToNode } from '../../main/utils/renProcess';
 import { useNavigate } from 'react-router-dom';
-import { OS_WINDOWS, OS_MAC } from '../../config/constants';
+import { OS_WINDOWS, OS_MAC, OS_LINUX } from '../../config/constants';
 const os = require('os');
 
 const Titlebar = () => {
@@ -14,18 +14,19 @@ const Titlebar = () => {
         <>
             <Box className={currOs === OS_WINDOWS ? "title-bar title-bar_windows" : "title-bar title-bar_unix"}>
                 <div className="tb-controls">
-
+                    {currOs === OS_LINUX &&
                         <div className="traffic-light">
                             <div onClick={() => sendMessageToNode('closeWindow')} className="close-unix">close</div>
                             <div onClick={() => sendMessageToNode('minimize')} className="mini-unix">minimise</div>
                             <div onClick={() => sendMessageToNode('maximize')} className="res-unix">resize</div>
                         </div>
+                    }
                     <Box className={currOs === OS_MAC ? "mac_back" : "tb-cntrl_windows"}
                         sx={{ visibility: window.history.length === 1 ? 'hidden' : 'inherit' }}
                         onClick={() => navigate(-1)}
                     >
-                        <Button color={currOs === OS_MAC ? "primary" : "inherit"} sx={{ minWidth: currOs === OS_MAC ? 'inherit' : null }} variant={currOs === OS_MAC ? 'contained' : 'text'}>
-                            {currOs === OS_MAC ?
+                        <Button color={currOs === OS_MAC || currOs === OS_LINUX ? "primary" : "inherit"} sx={{ minWidth: currOs === OS_MAC || currOs === OS_LINUX ? 'inherit' : null }} variant={currOs === OS_MAC || currOs === OS_LINUX ? 'contained' : 'text'}>
+                            {currOs === OS_MAC || currOs === OS_LINUX ?
                                 <ArrowBackIosNewOutlinedIcon sx={{ fontSize: 15 }} />
                                 :
                                 <ArrowBackOutlinedIcon sx={{ fontSize: 20 }} />
